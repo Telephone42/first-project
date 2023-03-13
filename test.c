fbeb28d1f349e904519ceed04a41daa3e66e96cd
telephone <1183385600@qq.com>
2023-03-14 00:46:19 +0800

    sub

--- test.c
+++ test.c
@@ -4,7 +4,9 @@
 int main(int argc, char const *argv[]) {
   int a = 10;
   int b = 20;
-  int c = add(a,b);
+  int c = add(a, b);
   printf("a+b = %d\n", c);/* code */
+  int d = sub(a, b);
+  printf("a-b = %d\n", d);
   return 0;
 }
